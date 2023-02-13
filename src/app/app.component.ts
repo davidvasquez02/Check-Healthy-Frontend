9ed7eaf9dfae7c1849bc958c4b16a7eee992fbcc
Rubi Utima <rubi2190032@correo.uis.edu.co>
2023-02-13 04:46:21 -0500

    suscrbcion a noti

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,20 +9,4 @@ import { mergeMap } from 'rxjs';
 })
 export class AppComponent {
   title = 'Check-Health';
-
-  constructor(private afMessaging: AngularFireMessaging) { 
-    this.getToken();
-  }
-  requestPermission() {
-    this.afMessaging.requestPermission
-      .pipe(mergeMap(() => (this.afMessaging.tokenChanges)))
-      .subscribe(
-        (token) => { console.log('Permission granted! Save to the server!', token); },
-        (error) => { console.error(error); },  
-      );
-  }
-
-  getToken(){
-    this.afMessaging.getToken.subscribe((res) => console.log('Token: ',res));
-  }
 }
--- src/app/components/regiter/regiter.component.ts
+++ src/app/components/regiter/regiter.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AngularFireMessaging } from '@angular/fire/compat/messaging';
+import { mergeMap } from 'rxjs';
 import { RegisterData } from 'src/app/models/register-data';
 
 @Component({
@@ -9,8 +11,25 @@ import { RegisterData } from 'src/app/models/register-data';
 export class RegiterComponent {
   registerData: RegisterData = new RegisterData('','','',0,0,'',false,false,false);
 
+  constructor(private afMessaging: AngularFireMessaging) { 
+    this.getToken();
+  }
+
   registraUsuario(){
     console.log(this.registerData)
+  }
+
+  requestPermission() {
+    this.afMessaging.requestPermission
+      .pipe(mergeMap(() => (this.afMessaging.tokenChanges)))
+      .subscribe(
+        (token) => { console.log('Permission granted! Save to the server!', token); },
+        (error) => { console.error(error); },  
+      );
+  }
+
+  getToken(){
+    this.afMessaging.getToken.subscribe((res) => console.log('Token: ',res));
   }
 }
  
