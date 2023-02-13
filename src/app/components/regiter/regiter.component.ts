4dff7f851bb595a6e0131bac3e9c4094dbb81c29
Rubi Utima <rubi2190032@correo.uis.edu.co>
2023-02-12 19:45:37 -0500

    add folder and some classes

---
+++ src/app/components/regiter/regiter.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-regiter',
+  templateUrl: './regiter.component.html',
+  styleUrls: ['./regiter.component.css']
+})
+export class RegiterComponent {
+
+}
---
+++ src/app/models/user.ts
@@ -0,0 +1,13 @@
+export class User {
+
+    public id!: number;
+    public username!: string;
+    public contrasena!: string;
+    public nombre!: string;
+    public correo!: string;
+    public foto!: String;
+
+    constructor(
+      
+    ){}
+}
