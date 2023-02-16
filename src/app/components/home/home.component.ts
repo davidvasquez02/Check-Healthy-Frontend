06149a79c60b53813f67de5feb2f1fa2edfade82
Rubi Utima <rubi2190032@correo.uis.edu.co>
2023-02-15 21:46:28 -0500

    enfermedades

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { EnfermedadUser } from 'src/app/models/enfermedadUser';
+import { EnfermedadUserService } from 'src/app/services/enfermedadUser.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+
+  enfermedades: EnfermedadUser[] = [];
+
+  constructor(
+    public enfermedadUserService: EnfermedadUserService
+  ) {}
+
+  ngOnInit(): void {
+    this.enfermedadUserService.getAllEnfermedades(parseInt(sessionStorage.getItem('idUsuario')!)).subscribe((response) => {
+      this.enfermedades = response;
+      console.log(response);
+
+      for (let e of this.enfermedades) {
+        if(e.idEnfermedad === 1 ){
+          e.route = '../control/db';
+        }else if(e.idEnfermedad === 2 ){
+          e.route = '../control/ht';
+        }else if(e.idEnfermedad === 3 ){
+          e.route = '../control/as';
+        };
+      }
+
+      console.log(this.enfermedades);
+    });
+  }
 
 }
---
+++ src/app/models/enfermedadUser.ts
@@ -0,0 +1,13 @@
+export class EnfermedadUser {
+    public id!: number;
+    public idUsuario!: number;
+    public idEnfermedad!: number;
+    public nombreUsuario!: string;
+    public nombreEnfermedad!: string;
+    public padece!: boolean;
+    public route!: string;
+
+    constructor(
+      
+    ){}
+}
