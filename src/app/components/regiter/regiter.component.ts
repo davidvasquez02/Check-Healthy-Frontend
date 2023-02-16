6957cacd37f1adcc24ad4fee58346710dc1046fa
Rubi Utima <rubi2190032@correo.uis.edu.co>
2023-02-16 01:36:48 -0500

    yeu

--- src/app/components/regiter/regiter.component.ts
+++ src/app/components/regiter/regiter.component.ts
@@ -9,47 +9,73 @@ import Swal from 'sweetalert2';
 @Component({
   selector: 'app-regiter',
   templateUrl: './regiter.component.html',
-  styleUrls: ['./regiter.component.css']
+  styleUrls: ['./regiter.component.css'],
 })
 export class RegiterComponent {
-  registerData: RegisterData = new RegisterData('','','', '',0,0,'',false,false,false);
+  registerData: RegisterData = new RegisterData(
+    '',
+    '',
+    '',
+    '',
+    0,
+    0,
+    '',
+    false,
+    false,
+    false
+  );
 
-  registerDataResponse: RegisterData = new RegisterData('','','', '',0,0,'',false,false,false);
+  registerDataResponse: RegisterData = new RegisterData(
+    '',
+    '',
+    '',
+    '',
+    0,
+    0,
+    '',
+    false,
+    false,
+    false
+  );
 
-
-  constructor(private afMessaging: AngularFireMessaging, private router: Router, private userService: UserService) { 
+  constructor(
+    private afMessaging: AngularFireMessaging,
+    private router: Router,
+    private userService: UserService
+  ) {
     this.getToken();
   }
 
-  registraUsuario(){
-    console.log(this.registerData)
-    this.userService.createUser(this.registerData).subscribe((response)=>{
+  registraUsuario() {
+    console.log(this.registerData);
+    this.userService.createUser(this.registerData).subscribe((response) => {
       this.registerDataResponse = response;
-      if(this.registerDataResponse.nombre === this.registerData.nombre){
+      if (this.registerDataResponse.nombre === this.registerData.nombre) {
         Swal.fire('Registro completado con Exito!', '', 'success');
         setTimeout(() => {
           sessionStorage.clear();
           this.router.navigate(['login']);
         }, 1000);
-        
-
       } else {
         Swal.fire('Ocurrio un problema', '', 'error');
       }
-    })
+    });
   }
 
   requestPermission() {
     this.afMessaging.requestPermission
-      .pipe(mergeMap(() => (this.afMessaging.tokenChanges)))
+      .pipe(mergeMap(() => this.afMessaging.tokenChanges))
       .subscribe(
-        (token) => { console.log('Permission granted! Save to the server!', token); },
-        (error) => { console.error(error); },  
+        (token) => {
+          console.log('Permission granted! Save to the server!', token);
+        },
+        (error) => {
+          console.error(error);
+        }
       );
   }
 
-  getToken(){
-    this.afMessaging.getToken.subscribe((res) => console.log('Token: ',res));
+  getToken() {
+    this.afMessaging.getToken.subscribe((res) => console.log('Token: ', res));
   }
 }
- 
