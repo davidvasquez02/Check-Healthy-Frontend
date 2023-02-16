85d2b82007a845c2e4c1faaa61fac504520b034a
Rubi Utima <rubi2190032@correo.uis.edu.co>
2023-02-16 06:48:27 -0500

    url

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: false,
-    urlBack: 'http://localhost:8080/CheckHealth',
+    urlBack: 'http://checkhealthback-env.eba-kwms4awv.us-east-1.elasticbeanstalk.com/api',
   };
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  urlBack: "http://localhost:8080/CheckHealth/api",
+  urlBack: 'http://checkhealthback-env.eba-kwms4awv.us-east-1.elasticbeanstalk.com/api',
 };
